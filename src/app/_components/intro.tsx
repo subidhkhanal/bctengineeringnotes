bb4e5846f789851ce5ce814293a2ab83775577b3
Subidh Khanal <subidhkhanal37@gmail.com>
2024-08-04 08:19:32 +0545

    updating navbar

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,16 +1,9 @@
 /** @format */
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+// import { useState } from "react";
 
 export function Intro() {
-  const [activeLink, setActiveLink] = useState("none");
-
-  function handlelink() {
-    setActiveLink("name");
-    console.log(activeLink);
-  }
-
   return (
     <section className="pl-6 mb-[45px] h-[100px] sticky bg-[#f7fafc] z-[1] font-medium text-base border-b-[#e3e8ee] border-b border-solid top-0">
       <div className="flex justify-between">
@@ -22,10 +15,7 @@ export function Intro() {
       <div className="h-12 flex items-center">
         <div className="flex justify-start custom-font">
           <h2 className="mr-6 font-medium text-sm text-[rgb(60,66,87)] hover:text-[rgb(0,0,0)]">
-            <Link href="/8thsem" onclick={handlelink()}>
-              {" "}
-              1st Semester
-            </Link>
+            <Link href="/8thsem"> 1st Semester</Link>
           </h2>
           <h2 className="mr-6 font-medium text-sm text-[rgb(60,66,87)] hover:text-[rgb(0,0,0)]">
             <Link href="/8thsem">2nd Semester</Link>
