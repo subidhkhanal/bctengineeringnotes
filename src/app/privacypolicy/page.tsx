05b6814ce5c5c7338ee334b525590d8b6d263c8d
Subidh Khanal <subidhkhanal37@gmail.com>
2024-08-22 17:30:12 +0545

    updates privacy policy

---
+++ src/app/privacypolicy/page.tsx
@@ -0,0 +1,54 @@
+/** @format */
+
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      <div className="mx-auto max-w-screen-lg rounded bg-white p-6 ">
+        <h1 className="mb-4 text-3xl font-bold">
+          Privacy Policy for BCT Notes
+        </h1>
+
+        <h2 className="mb-2 text-2xl font-semibold">Introduction</h2>
+        <p className="mb-4">
+          Thank you for choosing BCT Notes, an Engineering Notes website. We are
+          committed to protecting your privacy and ensuring the security of your
+          personal information. This Privacy Policy is designed to inform you
+          about how we collect, use, and safeguard your data.
+        </p>
+        <p className="mb-4">
+          Please take a moment to review this policy to understand our practices
+          regarding your personal information. By using our website, you consent
+          to the terms outlined in this Privacy Policy.
+        </p>
+
+        <h2 className="mb-2 text-2xl font-semibold">
+          We do not collect any personal information of our users.
+        </h2>
+
+        <h2 className="mb-2 text-2xl font-semibold">Contact Us</h2>
+        <p className="mb-2">
+          If you have any questions, concerns, or requests related to your
+          privacy or this Privacy Policy, please contact us at:
+        </p>
+        <ul className="mb-4 list-disc pl-6">
+          <li>
+            <strong>Email:</strong> &quot;engineeringstudent0012@gmail.com
+            &quot;
+          </li>
+        </ul>
+        <p>
+          We will respond to your inquiries promptly and work to address your
+          concerns.
+        </p>
+
+        <p className="mt-4">
+          This Privacy Policy was last updated on 2024-05-27. Any future updates
+          will be posted on this page, so please check back periodically to stay
+          informed about our data handling practices.
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/app/termsofservice/page.tsx
@@ -0,0 +1,145 @@
+/** @format */
+
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="mx-auto mt-10 max-w-3xl bg-white p-6 ">
+      <h2 className="mb-4 text-2xl font-bold">
+        BCT Notes Website Terms of Service
+      </h2>
+
+      <p className="mb-4">Effective Date: 2024-05-27</p>
+
+      <p>
+        Welcome to BCT NOtes These Terms of Service &ldquo;Terms&ldquo; govern
+        your use of our website and services. By accessing or using our website,
+        you agree to be bound by these Terms. Please read them carefully before
+        using the site.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">1. Acceptance of Terms</h3>
+
+      <p>
+        By accessing or using our website, you agree to comply with and be bound
+        by these Terms. If you do not agree with these Terms, please do not use
+        our website.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">2. Use of the Website</h3>
+
+      <p>
+        a. <span className="font-bold">Access:</span> You may access our website
+        for personal and non-commercial use.
+      </p>
+
+      <p>
+        c. <span className="font-bold">Prohibited Activities:</span> You shall
+        not use the website for any purpose that is unlawful or prohibited by
+        these Terms.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">3. Content</h3>
+
+      <p>
+        a. <span className="font-bold">User-Generated Content:</span> Our
+        website may allow users to post, upload, or submit content. You grant us
+        a non-exclusive, worldwide, royalty-free license to use, reproduce,
+        modify, adapt, publish, translate, and distribute such content.
+      </p>
+
+      <p>
+        b. <span className="font-bold">Our Content:</span> All content on our
+        website, unless otherwise indicated, is our property and is protected by
+        intellectual property laws.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">4. Privacy</h3>
+
+      <p>
+        Your use of the website is also governed by our Privacy Policy{" "}
+        <a href="/privacypolicy" className="text-blue-500 underline">
+          [link to privacy policy]
+        </a>
+        , which explains how we collect, use, and disclose your information.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">5. Termination</h3>
+
+      <p>
+        We reserve the right to terminate or suspend your access to the website
+        at any time, with or without cause, and without notice.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">6. Disclaimers</h3>
+
+      <p>
+        a. <span className="font-bold">Accuracy:</span> While we strive to
+        provide accurate and up-to-date information, we do not warrant the
+        accuracy, reliability, completeness, or usefulness of the content on our
+        website.
+      </p>
+
+      <p>
+        b. <span className="font-bold">Third-Party Links:</span> Our website may
+        contain links to third-party websites. These links are provided for your
+        convenience and do not imply our endorsement of the linked sites. We are
+        not responsible for the content of linked sites.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">7. Limitation of Liability</h3>
+
+      <p>
+        To the fullest extent permitted by law, we shall not be liable for any
+        indirect, incidental, special, consequential, or punitive damages, or
+        any loss of profits or revenues.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">8. Indemnification</h3>
+
+      <p>
+        You agree to indemnify and hold us harmless from and against any claims,
+        liabilities, damages, losses, and expenses arising out of or in
+        connection with your use of the website.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">9. Governing Law</h3>
+
+      <p>
+        These Terms shall be governed by and construed in accordance with the
+        laws of US. Any disputes arising under or in connection with these Terms
+        shall be subject to the exclusive jurisdiction of the courts located in
+        US.
+      </p>
+
+      <h3 className="mt-4 text-xl font-bold">10. Changes to Terms</h3>
+
+      <p>
+        We reserve the right to modify or update these Terms at any time. Any
+        changes will be effective upon posting on the website. Your continued
+        use of the website after any changes constitute your acceptance of the
+        updated Terms.
+      </p>
+
+      <p>
+        By using our website, you acknowledge that you have read, understood,
+        and agreed to these Terms of Service.
+      </p>
+
+      <p>
+        If you have any questions about these Terms, please contact us at
+        [Contact Information].
+      </p>
+
+      <p className="mt-4">
+        <strong>BCT Notes</strong>
+        <br />
+        <a href="/">www.bctengineeringnotes.com</a>
+        <br />
+        engineeringstudent0012@gmail.com
+        <br />
+        2024-05-27
+      </p>
+    </div>
+  );
+}
